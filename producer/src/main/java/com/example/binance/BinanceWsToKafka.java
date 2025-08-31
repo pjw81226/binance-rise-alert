0f927f390c77ded79924c9862ee60e49205f17d9
jwpark <woojang812@gmail.com>
2025-08-31 21:48:34 +0900

    chore(Kafka): remove unuse import

--- producer/src/main/java/com/example/binance/BinanceWsToKafka.java
+++ producer/src/main/java/com/example/binance/BinanceWsToKafka.java
@@ -1,6 +1,5 @@
 package com.example.binance;
 
-import com.example.binance.KafkaTopics;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URI;
