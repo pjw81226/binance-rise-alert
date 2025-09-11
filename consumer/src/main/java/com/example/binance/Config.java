4d541beaa7bc402ba072e01b973bd5c6dce89ff2
jwpark <woojang812@gmail.com>
2025-09-11 15:49:47 +0900

    revert config

--- consumer/src/main/java/com/example/binance/Config.java
+++ consumer/src/main/java/com/example/binance/Config.java
@@ -6,7 +6,7 @@ public class Config {
     public static final String CONSUMER_GROUP_ID = "discord-alert-consumer-group";
 
     public static String getDiscordWebhookUrl() {
-        String webhookUrl = System.getenv("DISCORD WEBHOOK URL");
+        String webhookUrl = System.getenv("Discord_Webhook_URL");
         if (webhookUrl == null || webhookUrl.isEmpty()) {
             throw new IllegalStateException("DISCORD WEBHOOK URL이 설정되지 않았습니다.");
         }
